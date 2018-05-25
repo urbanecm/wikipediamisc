9f86c3ffc71012391c0df218fce10130142addfe
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-25 10:00:47 +0200

    Always valid JSON

--- historypatrol.py
+++ historypatrol.py
@@ -4,6 +4,7 @@ import os
 import cgi
 import sys
 from wmflabs import db
+import json
 
 #Print header
 print 'Content-type: application/json\n'
@@ -15,10 +16,10 @@ if 'QUERY_STRING' in os.environ:
 	try:
 		rev_page = qs['rev_page'][0]
 	except:
-		print 'no rev_page'
+		print '{"error": "no rev_page"}'
 		sys.exit(0)
 else:
-	print 'no rev_page'
+	print '{"error": "no rev_page"}'
 	sys.exit(0)
 
 ##### PROGRAM ####
@@ -28,7 +29,7 @@ cur = conn.cursor()
 with cur:
 	sql = 'select rc_id from revision join recentchanges on rc_this_oldid=rev_id where rev_page=' + rev_page + ' and rc_patrolled=0;'
 	cur.execute(sql)
-	data = cur.fetchall()
-
-result = data[0][0]
-print result
+	result = []
+	for row in cur.fetchall():
+		result.append(row[0])
+	print json.dumps(result)
